951991a674281561f66d713f71d3233581c83864
4FCG <30472504+4FCG@users.noreply.github.com>
2017-09-27 19:56:54 +0200

    Completed automatic table generation

--- tables.js
+++ tables.js
@@ -20,12 +20,16 @@ connection.connect(function(err) {
 module.exports = {
   func: (Client, msg) => {
     Client.bot.guilds.array().forEach(function(guild){
-      let query = 'CREATE TABLE IF NOT EXISTS`' + guild.name + '` (`roles` VARCHAR(45) NOT NULL,';
+      let query = 'CREATE TABLE IF NOT EXISTS`' + guild.id + '` (`roles` VARCHAR(45) NOT NULL,';
       fs.readdirSync('./commands/').forEach(function(command){
-        query = query + '`' + command + '` INT ZEROFILL NOT NULL,';
+        query = query + '`' + command + '` INT NOT NULL DEFAULT 0,';
       });
       query = query + 'PRIMARY KEY (`roles`))COMMENT = "' + guild.name + ' table";';
       connection.query(query, function (error, results, fields) {});
+      guild.roles.array().forEach(function(role){
+        let query = 'INSERT IGNORE INTO `' + guild.id + '` (roles) VALUES ("' + role.name + '");';
+        connection.query(query, function (error, results, fields) {});
+      });
     });
   }
 }
