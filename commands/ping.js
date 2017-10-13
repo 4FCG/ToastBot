f021b2e4178b5794ac469d81d2f48f3fb54bb891
4FCG <snemmel.b@gmail.com>
2017-10-13 09:30:30 +0200

    Added ms counter to the ping command

--- commands/ping.js
+++ commands/ping.js
@@ -2,6 +2,8 @@ module.exports = {
     Description: 'Sends a pong back.',
     Usage: 'ping',
     func: (client, msg, args) => {
-        msg.channel.send('pong');
+        msg.channel.send('pong').then(message => {
+          message.edit(`pong, this took ${message.createdTimestamp - msg.createdTimestamp}ms`);
+        });
     }
 }
