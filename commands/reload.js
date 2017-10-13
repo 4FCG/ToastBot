8ab94158e4d56960ed77e30a07a8cc24ca21bafe
4FCG <snemmel.b@gmail.com>
2017-10-13 10:13:16 +0200

    fixed small bug

--- commands/reload.js
+++ commands/reload.js
@@ -2,7 +2,7 @@ module.exports = {
     Description: 'Command to reload other commands. Dev only.',
     Usage: 'reload <command>',
     func: (client, msg, args) => {
-        if (msg.author.id !== client.user.id) {return msg.reply("This command is dev only.");}
+        if (msg.author.id !== '206765034804609025') {return msg.reply("This command is dev only.");}
         if (args.length >= 1) {
           client.load.func(client, args[0]);
         }
