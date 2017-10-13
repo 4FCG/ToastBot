8b6ef8b5bba6ea0c14bfd0d079a7dadd6829454f
4FCG <snemmel.b@gmail.com>
2017-10-13 09:14:21 +0200

    Disabled reload from public access.

--- commands/reload.js
+++ commands/reload.js
@@ -1,7 +1,8 @@
 module.exports = {
-    Description: 'Command to reload other commands.',
+    Description: 'Command to reload other commands. Dev only.',
     Usage: 'reload <command>',
     func: (client, msg, args) => {
+        if (msg.author.id !== client.user.id) {return msg.reply("This command is dev only.");}
         if (args.length >= 1) {
           client.load.func(client, args[0]);
         }
